9a6b624fff5c3c2975703cd487030277106937c6
zync <chandankrishna288@gmail.com>
2025-06-08 14:25:59 +0530

    textscale changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-5">
-      <h1 className="text-4xl">Z3 Stack</h1>
+      <h1 className="text-h1">Z3 Stack</h1>
 
-      <h2 className="text-xl">
+      <h2 className="text-h6">
         Next.js Starter Template by{" "}
         <Link href={"https://github.com/zyncc"}>Zync</Link>
       </h2>
